d5f8e7711b633f7807cfb93dc75c2d0e79b24df2
Dmitry Korochkin <abramslost@gmail.com>
2023-01-16 10:57:25 +0300

    add index.js

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import './style.css'
+import {modal} from './modules/modal'
 
 
+modal();
---
+++ src/modules/modal.ts
@@ -0,0 +1,9 @@
+export const modal = () => {
+  const modalBlock = document.querySelector('.modal-block');
+  const modalClose = document.querySelector('.close');
+  const modalOpen = document.querySelector('.open')
+
+  console.log(modalBlock)
+  console.log(modalClose)
+  console.log(modalOpen)
+}
