39f85085457276c8c7e31472b3717c02540938bd
Dmitry Korochkin <abramslost@gmail.com>
2023-01-16 13:40:10 +0300

    add close and open to button

--- src/modules/modal.ts
+++ src/modules/modal.ts
@@ -1,9 +1,30 @@
-export const modal = () => {
-  const modalBlock = document.querySelector('.modal-block');
-  const modalClose = document.querySelector('.close');
-  const modalOpen = document.querySelector('.open')
+export const modal = (): void => {
+  const modalBlock: HTMLElement = document.querySelector('.modal-block') as HTMLElement;
+  const btnOpen: HTMLElement = document.querySelector('.btn-open') as HTMLElement;
+  const btnClose: HTMLElement = document.querySelector('.btn-close') as HTMLElement;
+  const btns: any = document.querySelectorAll('.btn');
 
-  console.log(modalBlock)
-  console.log(modalClose)
-  console.log(modalOpen)
+
+  btns.forEach((btn: Element) => {
+    btn.addEventListener('click', (e: Event) => {
+        e.preventDefault()
+    })
+
+    btnOpen.addEventListener('click', () => {
+      modalBlock.style.display = 'flex';
+    })
+    btnClose.addEventListener('click', () => {
+      modalBlock.style.display = 'none';
+    })
+  })
+  document.addEventListener("keydown", function (e) {
+    const key = e.key;
+    if (key === "Escape") {
+      btnClose.addEventListener('click', () => {
+        modalBlock.style.display = 'none';
+      })
+    }
+  });
+
+
 }
