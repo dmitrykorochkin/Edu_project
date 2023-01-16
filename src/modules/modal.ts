57c281334ef74b009be9b86a4aefd0ebb83eb0d8
Dmitry Korochkin <abramslost@gmail.com>
2023-01-16 14:47:58 +0300

    add modal

--- src/modules/modal.ts
+++ src/modules/modal.ts
@@ -2,12 +2,12 @@ export const modal = (): void => {
   const modalBlock: HTMLElement = document.querySelector('.modal-block') as HTMLElement;
   const btnOpen: HTMLElement = document.querySelector('.btn-open') as HTMLElement;
   const btnClose: HTMLElement = document.querySelector('.btn-close') as HTMLElement;
-  const btns: any = document.querySelectorAll('.btn');
+  const btns: NodeListOf<HTMLElement> = document.querySelectorAll('.btn');
 
 
   btns.forEach((btn: Element) => {
     btn.addEventListener('click', (e: Event) => {
-        e.preventDefault()
+      e.preventDefault()
     })
 
     btnOpen.addEventListener('click', () => {
@@ -16,15 +16,21 @@ export const modal = (): void => {
     btnClose.addEventListener('click', () => {
       modalBlock.style.display = 'none';
     })
-  })
-  document.addEventListener("keydown", function (e) {
-    const key = e.key;
-    if (key === "Escape") {
-      btnClose.addEventListener('click', () => {
+
+    document.addEventListener("keydown", function (e) {
+      const key = e.key;
+      if (key === "Escape") {
         modalBlock.style.display = 'none';
-      })
+      }
+    });
+
+    const closeModal = (e: Event) => {
+      const target = e.target as HTMLElement;
+      if(target.matches('.modal-block')) {
+        modalBlock.style.display = 'none';
+      }
     }
-  });
-
-
+    
+  })
+  
 }
