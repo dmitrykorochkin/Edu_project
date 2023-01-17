bf0d7d6b3f10363be7403acd59d0a2dc277e2243
Dmitry Korochkin <abramslost@gmail.com>
2023-01-17 20:12:21 +0300

    add forEach

--- src/modules/accordion.ts
+++ src/modules/accordion.ts
@@ -2,16 +2,19 @@ export const accordion = () => {
   const accordions: NodeListOf<HTMLElement> = document.querySelectorAll('.accordion_title');
  
 
-  for(let i = 0; i < accordions.length; i++) {
-    accordions[i].addEventListener('click', function(this: HTMLElement) {
-      this.classList.toggle('active');
-      const accordionText = (this.nextElementSibling as HTMLElement)
-      if(accordionText.style.maxHeight) {
-        accordionText.style.maxHeight = null
-      } else {
-        accordionText.style.maxHeight = accordionText.scrollHeight + 'px'
-      }
+    accordions.forEach(acc => {
+      acc.addEventListener('click', function(this: HTMLElement) {
+        this.classList.toggle('active');
+        const accordionText: HTMLElement = (this.nextElementSibling as HTMLElement)
+        if(accordionText.style.maxHeight)  {
+          (accordionText as HTMLElement).style.maxHeight = null
+        } else {
+          accordionText.style.maxHeight = accordionText.scrollHeight + 'px'
+        }
+      })
     })
-  }
+  
   
 }
+
+
