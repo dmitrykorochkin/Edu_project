aba0f28917448f505ff6b8c27597a4b615cd8db7
Dmitry Korochkin <abramslost@gmail.com>
2023-01-17 16:02:10 +0300

    add accordions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './style.css'
 import {modal} from './modules/modal'
+import {accordion} from './modules/accordion'
 
 
 modal();
+accordion();
--- src/modules/accordion.ts
+++ src/modules/accordion.ts
@@ -1,3 +1,17 @@
 export const accordion = () => {
+  const accordions: NodeListOf<HTMLElement> = document.querySelectorAll('.accordion_title');
+ 
+
+  for(let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener('click', function(this: HTMLElement) {
+      this.classList.toggle('active');
+      const accordionText = (this.nextElementSibling as HTMLElement)
+      if(accordionText.style.display === 'block') {
+        accordionText.style.display = 'none'
+      } else {
+        accordionText.style.display = 'block'
+      }
+    })
+  }
   
 }
